86858036604df2fa7cf8ed0f05ae6afc00d6889e
sean6 <sean6@DESKTOP-QRM65VO>
2022-11-20 23:21:06 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> list = Enumerable.Range(1, 10);
+			foreach (var item in list)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine();
+			list=Enumerable.Range(10, 6);
+			foreach (var item in list)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
